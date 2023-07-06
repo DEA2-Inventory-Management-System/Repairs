498fa5383af4d2c295952e936aa615d51d3f36df
Tharuka_00 <tharusenevi2000@gmail.com>
2023-07-06 19:19:54 +0530

    Updated all the classes

--- repair/src/main/java/com/example/repair/RepairRepository.java
+++ repair/src/main/java/com/example/repair/RepairRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface RepairRepository extends JpaRepository<Repair,Integer>, JpaSpecificationExecutor<Repair> {
-    //Iterable<Object> void findAll();
     Repair save(Repair repair);
 
     Repair findById(int id);
